a237bf91642b7dfdf7b7c07a6e8c12aaf129696e
Itamar Junior <cdajuniorf@gmail.com>
2024-08-23 15:05:26 -0300

    Adicionar controllers, middleware e models

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ mensagem: 'Acesso não autorizado' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(400).json({ mensagem: 'Token inválido' });
+    }
+};
